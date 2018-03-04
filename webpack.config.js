4aed4a17f8b3526c4584f463f64e739e8b1f8f44
peymancyb <paymanghazvini@yahoo.com>
2018-03-04 23:47:37 +0400

    Advanced react

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+
+const config = {
+  resolve:{
+    modules: [
+      path.resolve('./lib'),
+      path.resolve('./node_modules'),
+    ]
+  },
+  entry:['babel-polyfill','./lib/renderers/dom.js'],
+  output:{
+    path: path.resolve(__dirname,'public'),
+    filename: 'bundle.js'
+  },
+  module:{
+    rules:[
+      {test: /\.js$/,exclude:/node_modules/, use:'babel-loader'}
+    ]
+  },
+};
+
+
+module.exports = config;
